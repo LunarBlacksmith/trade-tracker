374cb9552c0defa7876c6adff8bc0d45958c0d7b
Lunar <lunarblacksmith@gmail.com>
2025-09-29 16:14:59 +1000

    Changed from .NET Framework 4.8.1 to .NET 9.0.9. - NuGet packages such as 'Microsoft.EntityFrameworkCore.Sqlite' require .NET 8+.

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,12 +6,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace trade_tracker
+namespace trade_tracker;
+
+/// <summary>
+///		Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
 }
